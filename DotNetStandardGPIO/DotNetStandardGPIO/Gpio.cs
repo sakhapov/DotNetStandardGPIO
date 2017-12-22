ea4cccea0ca21703b8494bbf6f6b529193d88581
Ildar Sakhapov <i.sakhapov@hotmail.com>
2017-12-22 11:51:25 +0300

    Added GpioOn/Off helper

---
+++ DotNetStandardGPIO/DotNetStandardGPIO/Gpio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DotNetStandardGPIO
+{
+    public static class Gpio
+    {
+        public static void GpioOn(this int gpioId, bool enable = false)
+        {
+            if (CreateGpio(gpioId, false))
+                File.WriteAllText("/sys/class/gpio/gpio" + gpioId + "/value", enable ? "1" : "0");
+        }
+
+        private static bool CreateGpio(int gpioId, bool gpioIn)
+        {
+            if (Directory.Exists("/sys/class/gpio/gpio" + gpioId)) return true;
+
+            try
+            {
+                File.WriteAllText("/sys/class/gpio/export", gpioId.ToString());
+                if (!gpioIn)
+                {
+                    File.WriteAllText("/sys/class/gpio/gpio" + gpioId + "/direction", "out");
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot export or specify GPIO" + gpioId + " as IN/OUT, are you root? " + e);
+                return false;
+            }
+        }
+    }
+}
---
+++ DotNetStandardGPIO/DotNetStandardGPIO/GpioMap.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetStandardGPIO
+{
+    public class GpioMap
+    {
+    }
+}
